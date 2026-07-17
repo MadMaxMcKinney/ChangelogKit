d8b18ddb114b01628fae29e732ad6ce9f650eb25
Max McKinney <max@maxmckinney.com>
2026-07-16 20:53:18 -0500

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version: 6.4
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ChangelogKit",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "ChangelogKit",
+            targets: ["ChangelogKit"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "ChangelogKit",
+            swiftSettings: [
+                .enableUpcomingFeature("ApproachableConcurrency"),
+            ],
+        ),
+        .testTarget(
+            name: "ChangelogKitTests",
+            dependencies: ["ChangelogKit"],
+            swiftSettings: [
+                .enableUpcomingFeature("ApproachableConcurrency"),
+            ],
+        ),
+    ],
+    swiftLanguageModes: [.v6]
+)
